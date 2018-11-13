9aeeac912098f3fb80e36dad9598b006ec0c1067
crzyivo <ivan.nadie@gmail.com>
2018-11-13 08:18:54 +0100

    Merge leftovers

--- workspace/PH_01_Placa/Debug/8led.d
+++ workspace/PH_01_Placa/Debug/8led.d
@@ -1,7 +1,7 @@
 8led.d 8led.o: ../8led.c ../8led.h \
  D:\PH\workspace\PH_01_Placa\common/44b.h \
  D:\PH\workspace\PH_01_Placa\common/option.h \
- D:\PH\workspace\PH_01_Placa\common/44blib.h
+ D:\PH\workspace\PH_01_Placa\common/44blib.h ../latido.h
 
 ../8led.h:
 
@@ -10,3 +10,5 @@ D:\PH\workspace\PH_01_Placa\common/44b.h:
 D:\PH\workspace\PH_01_Placa\common/option.h:
 
 D:\PH\workspace\PH_01_Placa\common/44blib.h:
+
+../latido.h:
